8b128ace807b2c4a3713efb2220857e9b996f0a7
Ivan Yuzvyshyn <iyuzv@softserveinc.com>
2018-10-05 10:13:28 +0300

    add chat to modern programming

---
+++ JustChat/JustChat/hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace JustChat.hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+    }
+}
